f4cb6f0c3e67e0234afdc191c3f48a23752a1ac3
Rikkokiri <pilvi.rajala@gmail.com>
2017-05-12 19:52:24 +0300

    Play button for playing the animation added

--- states/animationState.js
+++ states/animationState.js
@@ -4,42 +4,34 @@
 
 function AnimationState() {}
 
-var graphics;
-var centerX;
-var centerY;
-
 AnimationState.prototype = {
 
   create: function() {
 
     // Create the graphics objects and so on...
     this.createEssentials();
-    // Music!
-    music = this.game.add.audio('sail');
-    music.play();
-
 
     introSquare = new SolidSquare(100, 100, 50, "0xffffff");
 
 
-
-  },
-
-  update: function() {
-
-    graphics.clear();
-
-    introSquare.drawSquare(graphics);
+    // Finally... Music!
+    music = this.game.add.audio('sail');
+    music.play();
 
   },
 
   createEssentials: function() {
 
-    graphics = this.game.add.graphics(0,0);
-    centerX = this.game.width / 2;
-    centerY = this.game.height / 2;
+    this.graphics = this.game.add.graphics(0,0);
+    this.centerX = this.game.width / 2;
+    this.centerY = this.game.height / 2;
 
   }
 
+}
+
+AnimationState.prototype.update = function() {
+
+  this.graphics.clear();
 
 }
--- states/readyState.js
+++ states/readyState.js
@@ -4,17 +4,32 @@
 
 function ReadyState() {}
 
+var playButton;
+
 ReadyState.prototype = {
+
+  preload: function() {
+
+    this.game.load.spritesheet('playbutton', 'resources/playbutton.png');
+
+  },
 
   create: function() {
 
     console.log("ReadyState active");
 
-    // Start the actual animation
-    this.game.state.start('animation');
-  }
+    // Create the play button
+    playButton = this.game.add.button(this.game.width / 2, this.game.height / 2, 'playbutton', this.onPlayButtonClick);
+
+    //  setting the anchor to the center
+    playButton.anchor.setTo(0.5, 0.5);
+
+  },
 
   // TODO update?
   // When the play button is pushed, move to the next state
+  onPlayButtonClick: function() {
+    this.game.state.start('animation');
+  }
 
 };
