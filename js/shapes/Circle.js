4d6f29a1fdcc8191915ebf75029f91c9f1a01eeb
Rikkokiri <pilvi.rajala@gmail.com>
2017-05-15 21:58:30 +0300

    Documentation to Circle class

--- js/shapes/Circle.js
+++ js/shapes/Circle.js
@@ -5,23 +5,31 @@
 /**
  * Creates a new circle.
  *
- * Precondition: Diameter, centerX and centerY must be provided.
+ * @class Circle
+ * @constructor
  *
- * @param {Number} diameter - Diamater of the circle.
- * @param {Number} centerX - X-coordinate of the center of the circle.
- * @param {Number} centerY - Y-coordinate of the center of the circle.
- * @param {String} lineColor - Color of the line that is used to draw the circle. Must be a valid hexadecimal number. If undefined, default 0x000000.
- * @param {Number} lineWidth - Width of the line that is used to draw the circle. If undefined, width is set to 1.
- * @param {Boolean} nofill - If true, circle is filled with the linecolor, else no fill.
- *                           If left undefined, no fill.
+ * @param {Number} centerX - X-coordinate of the center of the circle. [undefined -> 0]
+ * @param {Number} centerY - Y-coordinate of the center of the circle. [undefined -> 0]
+ * @param {Number} diameter - Diamater of the circle. [undefined -> 0]
+ * @param {String} color - Color of the line that is used to draw the circle. Must be a valid hexadecimal number. If undefined, default 0xff0000.
+ * @param {Number} linewidth - Width of the line that is used to draw the circle. If undefined, width is set to 1.
+ * @param {Boolean} fill - If true, circle is filled with the color, else no fill.
+ *                           If left undefined, fill.
  */
-function Circle (centerX, centerY, diameter, lineColor, lineWidth, nofill) {
+function Circle (centerX, centerY, diameter, color, linewidth, fill) {
+
+  if (centerX === undefined) { centerX = 0; }
+  if (centerY === undefined) { centerY = 0; }
+  if (diameter === undefined) { diameter = 0; }
+  if (color === undefined) { color = 0xff0000; }
+  if (linewidth === undefined) { linewidth = 1; }
+  if (fill === undefined) { fill = true; }
 
   this.diameter = diamater;
   this.center = new Phaser.Point(centerX, centerY);
-  this.lineColor = (lineColor === undefined) ? 0x000000 : lineColor;
-  this.lineWidth = (lineWidth === undefined) ? 1 : lineWidth;
-  this.nofill = (nofill === undefined) ? true : nofill;
+  this.color = color;
+  this.linewidth = linewidth;
+  this.fill = fill;
 
   // Create the actual circle
   this.circle = new Phaser.Circle(this.center.x, this.center.y, this.diameter);
@@ -33,20 +41,21 @@ Solidcircle.prototype = {
 
   /**
    * Draw the circle.
-   *
    * @param {} graphics - The graphics object that is used for drawing the circle.
    */
    drawCircle: function(graphics) {
-     if(nofill){
 
-       // TODO
-
-     }
-     // If the circle is filled with the linecolor
-     else {
-       graphics.beginFill(this.lineColor);
+     // If the circle is filled with the color
+     if(fill){
+       graphics.beginFill(this.color);
        graphics.drawCircle(this.circle.x, this.circle.y, this.circle.diameter);
        graphics.endFill();
+
+     }
+     else {
+
+
+
      }
    },
 
@@ -106,11 +115,11 @@ Solidcircle.prototype = {
 
    // TODO Check color code validity?
    /**
-    * @param {Hexadecimal} newColor - The new linecolor of the circle as a hexadecimal value.
+    * @param {Hexadecimal} newColor - The new color of the circle as a hexadecimal value.
     * The value must be a valid hexadecimal.
     */
-   setLineColor: function(newColor){
-     this.linecolor = newColor;
+   setcolor: function(newColor){
+     this.color = newColor;
    },
 
    // ------- Shrink and expand circle --------------
@@ -138,6 +147,16 @@ Solidcircle.prototype = {
      // TODO
 
    }
+
+
+   // --- OTHER METHOD IDEAS ---
+
+   /**
+    * Calculate the sidelength of the largest square that can fit inside the circle
+    *
+    * Another method for getting coordinates for such square?
+    */
+
 
 
 }
