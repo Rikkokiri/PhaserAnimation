d3c15cd1e7a7fb4f2f8f6ed946c7662fad68bcfe
Rikkokiri <pilvi.rajala@gmail.com>
2017-05-17 17:33:52 +0300

    Creating little squares fixed

--- js/animation-phases/oneSquare.js
+++ js/animation-phases/oneSquare.js
@@ -9,8 +9,8 @@ var cY; // Y-coordinate of the center of the canvas
 
 var oneSquare = 0;
 var squareColor = 0xffffff;
-var explosionSize;
-var littleSquares;
+var explosionSize = 200;
+var littleSquares = [];
 
 /*
  * Prepare for the animation sequence by defining some regularly used values
@@ -24,12 +24,12 @@ function prepareOneSquareAnimation(gameWidth, gameHeight){
 
   // Briefly set oneSquare to state it will be in when little squares are added to animation
   // Create the little squares based on this information
-  oneSquare = new Square(0, 0, 200, squareColor);
+  oneSquare = new Square(0, 0, explosionSize, squareColor, 1, true);
   oneSquare.centerOn(cX, cY);
   createLittleSquares();
 
   // Make oneSquare disappear (make it tiny);
-  oneSquare.setSize(0);
+  oneSquare.setSize(1);
 
 }
 
@@ -43,26 +43,27 @@ function createLittleSquares(){
   var startingY = oneSquare.getPoint(0).y;
   var startingX = oneSquare.getPoint(0).x;
 
-  var endingY = oneSquare.getPoint(1).y - littlesize;
-  var endingX = oneSquare.getPoint(1).x - littlesize;
+  var endingY = oneSquare.getPoint(2).y - littlesize;
+  var endingX = oneSquare.getPoint(2).x - littlesize;
 
-  littleSquares = [];
 
-  for(var y = startingY; y < endingY; y += littlesize){
+  for(var y = startingY; y <= endingY; y += littlesize){
 
     var row = (y - startingY) % littlesize;
-    (littleSquares[row] === undefined) ? littleSquares = [];
+    console.log("Row:", row);
 
-    for(var x = startingX; x < endingX; x += littlesize){
+    if(littleSquares[row] === undefined){
+      littleSquares[row] = [];
+    }
+
+    for(var x = startingX; x <= endingX; x += littlesize){
       // function Square (upperLeftX, upperLeftY, sidelength, color, linewidth, fill);
       littleSquares[row].push(new Square(x, y, littlesize, squareColor, 0, true));
     }
   }
+  console.log("Squares created", littleSquares);
 }
 
-/*
- *
- */
-function expandTheSquare(amount){
-
+function restoreOneSquareColor(){
+  oneSquare.setColor(squareColor);
 }
